2e38ac17c5389ada2b3c50eb1e62b54ccc50977b
João Victtor S. Melo <69211741+JoaoVicttorsMelo@users.noreply.github.com>
2020-12-14 22:50:40 -0300

    I added a command of voice in my project

--- main.py
+++ main.py
@@ -1,65 +1,70 @@
+import pyttsx3
 import speech_recognition as sr
 import time as tm
 
-i = 0
+speak = pyttsx3.init('sapi5')
+r = sr.Recognizer()
+while True:
 
-while i == 0:
-    r = sr.Recognizer()
     with sr.Microphone() as source:
 
         print("have a conversation with the machine: ")
         order = r.listen(source)
         try:
             Order = r.recognize_google(order, language='eng-EUA')
-            print('Your order was: ' + Order)
+            speak.say('Your order was: ' + Order)
 
             if Order == "hello":
                 tm.sleep(1)
-                print("Hello, how are you?")
+                speak.say("Hello, how are you?")
+                speak.runAndWait()
 
             if Order == "I'm fine thanks how about you":
                 tm.sleep(1)
-                print("I'm great thanks, how can I help?")
+                speak.say("I'm great thanks, how can I help?")
+                speak.runAndWait()
 
             if Order == "I want to do an addition":
-                print("tell me the first number: ")
+                speak.say("tell me the first number: ")
                 num1 = r.listen(source)
-                print("tell me the second number: ")
+                speak.say("tell me the second number: ")
                 num2 = r.listen(source)
                 number_1 = r.recognize_google(num1, language='eng-EUA')
                 number_2 = r.recognize_google(num2, language='eng-EUA')
-                print("the addition value is = ")
-                print(int(number_1) + int(number_2))
+                speak.say("the addition value is = ")
+                x = int(number_1) + int(number_2)
+                speak.say(str(x))
 
             if Order == "I want to do a multiplication":
-                print("tell me the first number: ")
+                speak.say("tell me the first number: ")
                 num1 = r.listen(source)
-                print("tell me the second number: ")
+                speak.say("tell me the second number: ")
                 num2 = r.listen(source)
                 number_1 = r.recognize_google(num1, language='eng-EUA')
                 number_2 = r.recognize_google(num2, language='eng-EUA')
-                print("the multiplication value is = ")
-                print(int(number_1) * int(number_2))
+                speak.say("the multiplication value is = ")
+                x = int(number_1) + int(number_2)
+                speak.say(str(x))
 
             if Order == "I want to do a subtraction":
-                print("tell me the first number: ")
+                speak.say("tell me the first number: ")
                 num1 = r.listen(source)
-                print("tell me the second number: ")
+                speak.say("tell me the second number: ")
                 num2 = r.listen(source)
                 number_1 = r.recognize_google(num1, language='eng-EUA')
                 number_2 = r.recognize_google(num2, language='eng-EUA')
-                print("the subtraction value is = ")
-                print(int(number_1) - int(number_2))
-
+                speak.say("the subtraction value is = ")
+                x = int(number_1) + int(number_2)
+                speak.say(str(x))
             if Order == "I want to do a division":
-                print("tell me the first number: ")
+                speak.say("tell me the first number: ")
                 num1 = r.listen(source)
-                print("tell me the second number: ")
+                speak.say("tell me the second number: ")
                 num2 = r.listen(source)
                 number_1 = r.recognize_google(num1, language='eng-EUA')
                 number_2 = r.recognize_google(num2, language='eng-EUA')
-                print("the division value is = ")
-                print(int(number_1) / int(number_2))
-
+                speak.say("the division value is = ")
+                x = int(number_1) + int(number_2)
+                speak.say(str(x))
         except:
-            print("...")
+            speak.say("i don't understood")
